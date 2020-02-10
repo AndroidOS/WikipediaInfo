d4306b4327808b6154c321081386a79d49442ab2
AndroidOS <burrowsgit@gmail.com>
2020-02-11 09:36:45 +1100

    Added TableView

--- WikipediaInfo/AppDelegate.swift
+++ WikipediaInfo/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        NSApp.keyWindow?.contentMinSize = NSSize(width: 1000.0, height: 400.0)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
